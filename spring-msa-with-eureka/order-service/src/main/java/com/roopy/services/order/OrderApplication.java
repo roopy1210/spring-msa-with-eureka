489cd3df454901168e1b12177aae3319ae8f472d
pncrm <pncrm@DESKTOP-GKHI4QK>
2019-11-06 00:18:03 +0900

    주문,상품,결제 초기버전

---
+++ spring-msa-with-eureka/order-service/src/main/java/com/roopy/services/order/OrderApplication.java
@@ -0,0 +1,21 @@
+package com.roopy.services.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
